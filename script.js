d80f9321f342f954f2c2e1f060209a89e543e1ca
budgiemint <70620754+snowythebudg@users.noreply.github.com>
2025-01-14 09:00:38 -0700

    added files

---
+++ script.js
@@ -0,0 +1,11 @@
+function topnav1() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+
