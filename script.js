a13525a87ce383ac3faa6c8d75b103d00e831fbb
budgiemint <snowythebudg@gmail.com>
2025-01-15 18:22:46 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-function topnav1() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
-
-
+console.log('Happy developing ✨')
